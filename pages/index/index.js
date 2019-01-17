24dc457494e32dfbe908e84130e8fbe182b1fe00
Crawford <907000795@qq.com>
2019-01-17 15:27:49 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,85 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello 良小陈',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imgs: [
+      {
+        url: '../../static/img/nc-banner-1.png',
+        id: 1
+      },
+      {
+        url: '../../static/img/nc-banner-2.png',
+        id: 2
+      }
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    products: [
+      {
+        name: '狗狗理财1',
+        interest: '10.00%',
+        type: '商业承兑汇票',
+        balance: '1000.00',
+        id: 1
+      },
+      {
+        name: '狗狗理财2',
+        interest: '6.00%',
+        type: '商业承兑汇票',
+        balance: '10000.00',
+        id: 2
+      },
+      {
+        name: '狗狗理财3',
+        interest: '7.00%',
+        type: '商业承兑汇票',
+        balance: '9998.00',
+        id: 3
+      },
+      {
+        name: '狗狗理财4',
+        interest: '8.00%',
+        type: '商业承兑汇票',
+        balance: '4567.00',
+        id: 4
+      },
+      {
+        name: '狗狗理财5',
+        interest: '9.50%',
+        type: '商业承兑汇票',
+        balance: '1000.00',
+        id: 5
+      },
+      {
+        name: '狗狗理财6',
+        interest: '10.00%',
+        type: '商业承兑汇票',
+        balance: '1000.00',
+        id: 6
+      },
+      {
+        name: '狗狗理财7',
+        interest: '10.25%',
+        type: '商业承兑汇票',
+        balance: '1000.00',
+        id: 7
+      },
+      {
+        name: '狗狗理财8',
+        interest: '9.20%',
+        type: '商业承兑汇票',
+        balance: '1000.00',
+        id: 8
+      },
+      {
+        name: '狗狗理财9',
+        interest: '8.00%',
+        type: '商业承兑汇票',
+        balance: '1000.00',
+        id: 9
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +90,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,36 @@
 //logs.js
-const util = require('../../utils/util.js')
+const util = require('../../utils/util.js');
 
 Page({
   data: {
-    logs: []
+    imgUrls: [
+      'https://nc-banners.oss-cn-beijing.aliyuncs.com/nc-banner-1.png',
+      'https://nc-banners.oss-cn-beijing.aliyuncs.com/nc-banner-2.png'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
-  onLoad: function () {
+  onLoad: function() {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
+        return util.formatTime(new Date(log));
       })
-    })
+    });
+  },
+  takePhoto() {
+    const ctx = wx.createCameraContext();
+    ctx.takePhoto({
+      quality: 'high',
+      success: res => {
+        this.setData({
+          src: res.tempImagePath
+        });
+      }
+    });
+  },
+  error(e) {
+    console.log(e.detail);
   }
-})
+});
